f784d564dcb2babbceb2047e1ff886060e9adf7c
Harpo Roeder <roederharpo@protonmail.ch>
2023-04-26 15:16:43 -0500

    more lint on add_token

--- neopass_flutter/lib/pages/add_token.dart
+++ neopass_flutter/lib/pages/add_token.dart
@@ -1,93 +1,91 @@
-import 'dart:convert';
+import 'dart:convert' as convert;
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' as Services;
+import 'package:flutter/services.dart' as services;
 
 import 'automated_verification.dart';
-import '../main.dart' as Main;
+import '../main.dart' as main;
 
 class AddTokenPage extends StatelessWidget {
-  final Main.ClaimInfo claim;
+  final main.ClaimInfo claim;
 
   const AddTokenPage({Key? key, required this.claim}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final token = base64Url.encode(claim.pointer.system.key);
+    final token = convert.base64Url.encode(claim.pointer.system.key);
 
     return Scaffold(
       appBar: AppBar(
-        title: Main.makeAppBarTitleText('Add Token'),
+        title: main.makeAppBarTitleText('Add Token'),
       ),
       body: Container(
-        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.only(left: 5.0, right: 5.0),
         child: Column(
           children: [
             const SizedBox(height: 20),
-            Align(
+            const Align(
               alignment: AlignmentDirectional.centerStart,
-              child: Container(
-                margin: const EdgeInsets.only(left: 5.0),
-                child: const Text(
-                  "Token",
-                  style: TextStyle(
-                    fontFamily: 'inter',
-                    fontWeight: FontWeight.w300,
-                    fontSize: 14,
-                    color: Colors.white,
-                  ),
+              child: Text(
+                "Token",
+                style: TextStyle(
+                  fontFamily: 'inter',
+                  fontWeight: FontWeight.w300,
+                  fontSize: 14,
+                  color: Colors.white,
                 ),
               ),
             ),
             const SizedBox(height: 5),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  backgroundColor: Main.tokenColor,
-                  foregroundColor: Colors.black,
-                ),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 5),
-                    Text(token,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'inter',
-                          fontWeight: FontWeight.w300,
-                          fontSize: 14,
-                        )),
-                    const SizedBox(height: 10),
-                    const Align(
-                      alignment: AlignmentDirectional.centerEnd,
-                      child: Text(
-                        "Tap to copy",
-                        style: TextStyle(
-                          fontFamily: 'inter',
-                          fontWeight: FontWeight.w300,
-                          fontSize: 14,
-                          color: Colors.grey,
-                        ),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                backgroundColor: main.tokenColor,
+                foregroundColor: Colors.black,
+              ),
+              child: Column(
+                children: [
+                  const SizedBox(height: 5),
+                  Text(token,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'inter',
+                        fontWeight: FontWeight.w300,
+                        fontSize: 14,
+                      )),
+                  const SizedBox(height: 10),
+                  const Align(
+                    alignment: AlignmentDirectional.centerEnd,
+                    child: Text(
+                      "Tap to copy",
+                      style: TextStyle(
+                        fontFamily: 'inter',
+                        fontWeight: FontWeight.w300,
+                        fontSize: 14,
+                        color: Colors.grey,
                       ),
                     ),
-                    const SizedBox(height: 5),
-                  ],
-                ),
-                onPressed: () async {
-                  await Services.Clipboard.setData(
-                      Services.ClipboardData(text: token));
-
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(const SnackBar(content: Text('Copied')));
-                },
+                  ),
+                  const SizedBox(height: 5),
+                ],
               ),
+              onPressed: () async {
+                await services.Clipboard.setData(
+                    services.ClipboardData(text: token));
+
+                if (!context.mounted) {
+                  return;
+                }
+
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(const SnackBar(content: Text('Copied')));
+              },
             ),
             const SizedBox(height: 450),
             Align(
               alignment: AlignmentDirectional.center,
               child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Main.blueButtonColor,
+                    backgroundColor: main.blueButtonColor,
                     shape: const StadiumBorder(),
                   ),
                   child: const Text(
@@ -99,9 +97,9 @@ class AddTokenPage extends StatelessWidget {
                       color: Colors.white,
                     ),
                   ),
-                  onPressed: () async {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute<dynamic>(
+                        builder: (BuildContext context) {
                       return AutomatedVerificationPage(
                         claim: claim,
                       );
