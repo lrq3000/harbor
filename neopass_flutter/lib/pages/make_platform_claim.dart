49bdb23a59f9c56b459ff8b3c7d97f64393de035
Harpo Roeder <roederharpo@protonmail.ch>
2023-05-10 14:40:09 -0500

    place make_platform_claim options into SingleChildScrollView

--- neopass_flutter/lib/pages/make_platform_claim.dart
+++ neopass_flutter/lib/pages/make_platform_claim.dart
@@ -29,59 +29,61 @@ class _MakePlatformClaimPageState extends State<MakePlatformClaimPage> {
         title: main.makeAppBarTitleText('Make Claim'),
       ),
       body: Container(
-        padding: main.scaffoldPadding,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 75),
-            main.claimTypeToImage(widget.claimType),
-            const SizedBox(height: 75),
-            Text(
-              widget.claimType,
-              style: const TextStyle(
-                fontSize: 30,
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(height: 100),
-            main.LabeledTextField(
-              controller: textController,
-              title: "Profile information",
-              label: "Profile handle",
-            ),
-            const SizedBox(height: 150),
-            Align(
-              alignment: AlignmentDirectional.center,
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: main.blueButtonColor,
-                    shape: const StadiumBorder(),
+          padding: main.scaffoldPadding,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 75),
+                main.claimTypeToImage(widget.claimType),
+                const SizedBox(height: 75),
+                Text(
+                  widget.claimType,
+                  style: const TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
                   ),
-                  child: const Text('Next step'),
-                  onPressed: () async {
-                    if (textController.text.isEmpty) {
-                      return;
-                    }
-                    final claim = await main.makePlatformClaim(
-                        state.db,
-                        identity.processSecret,
-                        widget.claimType,
-                        textController.text);
-                    await state.mLoadIdentities();
+                ),
+                const SizedBox(height: 100),
+                main.LabeledTextField(
+                  controller: textController,
+                  title: "Profile information",
+                  label: "Profile handle",
+                ),
+                const SizedBox(height: 150),
+                Align(
+                  alignment: AlignmentDirectional.center,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: main.blueButtonColor,
+                        shape: const StadiumBorder(),
+                      ),
+                      child: const Text('Next step'),
+                      onPressed: () async {
+                        if (textController.text.isEmpty) {
+                          return;
+                        }
+                        final claim = await main.makePlatformClaim(
+                            state.db,
+                            identity.processSecret,
+                            widget.claimType,
+                            textController.text);
+                        await state.mLoadIdentities();
 
-                    if (context.mounted) {
-                      Navigator.push(context,
-                          MaterialPageRoute<AddTokenPage>(builder: (context) {
-                        return AddTokenPage(
-                          claim: claim,
-                        );
-                      }));
-                    }
-                  }),
+                        if (context.mounted) {
+                          Navigator.push(context,
+                              MaterialPageRoute<AddTokenPage>(
+                                  builder: (context) {
+                            return AddTokenPage(
+                              claim: claim,
+                            );
+                          }));
+                        }
+                      }),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
