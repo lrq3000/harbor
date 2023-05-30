c9d7084bf1c3752a270e440da3aba28d052fe5de
Harpo Roeder <roederharpo@protonmail.ch>
2023-05-30 09:21:33 -0500

    correct verifier url again

--- neopass_flutter/lib/api_methods.dart
+++ neopass_flutter/lib/api_methods.dart
@@ -77,7 +77,7 @@ Future<void> requestVerification(
   protocol.Pointer pointer,
   String claimType,
 ) async {
-  final url = "https://verifiers.grayjay.app/verifiers/"
+  final url = "https://verifiers.grayjay.app/"
       "${claimType.toLowerCase()}"
       "/api/v1/vouch";
 
