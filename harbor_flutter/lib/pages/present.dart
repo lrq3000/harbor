e2f5a4d6984cd78d542af7b8212f987ecec4db42
Harpo Roeder <roederharpo@protonmail.ch>
2023-09-18 14:06:27 -0500

    fix present page qr code showing wrong link

--- harbor_flutter/lib/pages/present.dart
+++ harbor_flutter/lib/pages/present.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/services.dart' as services;
 import 'package:share_plus/share_plus.dart' as share_plus;
 import 'package:flutter/material.dart';
@@ -9,16 +7,12 @@ import 'package:provider/provider.dart';
 import '../main.dart' as main;
 import '../models.dart' as models;
 import '../shared_ui.dart' as shared_ui;
-import '../protocol.pb.dart' as protocol;
 
 Future<void> handlePresentClipboard(
   final main.PolycentricModel state,
-  final protocol.Pointer pointer,
+  final String link,
   final BuildContext context,
 ) async {
-  final link = await main.makeEventLink(
-      state.db, pointer.system, pointer.process, pointer.logicalClock);
-
   await services.Clipboard.setData(services.ClipboardData(text: link));
 
   if (context.mounted) {
@@ -28,15 +22,12 @@ Future<void> handlePresentClipboard(
 
 Future<void> handlePresentShare(
   final main.PolycentricModel state,
-  final protocol.Pointer pointer,
+  final String link,
 ) async {
-  final link = await main.makeEventLink(
-      state.db, pointer.system, pointer.process, pointer.logicalClock);
-
   share_plus.Share.share(link);
 }
 
-class PresentPage extends StatelessWidget {
+class PresentPage extends StatefulWidget {
   final int identityIndex;
   final int claimIndex;
 
@@ -45,104 +36,135 @@ class PresentPage extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<PresentPage> createState() => _PresentPageState();
+}
+
+class _PresentPageState extends State<PresentPage> {
+  String? _link;
+
+  @override
+  void initState() {
+    super.initState();
+    loadState();
+  }
+
+  Future<void> loadState() async {
+    final state = Provider.of<main.PolycentricModel>(context, listen: false);
+    final identity = state.identities[widget.identityIndex];
+    final claim = identity.claims[widget.claimIndex];
+    final pointer = claim.pointer;
+
+    final link = await main.makeEventLink(
+        state.db, pointer.system, pointer.process, pointer.logicalClock);
+
+    setState(() {
+      _link = link;
+    });
+  }
+
+  @override
   Widget build(final BuildContext context) {
     final state = context.watch<main.PolycentricModel>();
-    final identity = state.identities[identityIndex];
-    final claim = identity.claims[claimIndex];
-    final encodedClaim = base64.encode(claim.pointer.writeToBuffer());
-    final pointer = claim.pointer;
+    final identity = state.identities[widget.identityIndex];
+    final claim = identity.claims[widget.claimIndex];
 
     return shared_ui.StandardScaffold(
       appBar: AppBar(
         title: shared_ui.makeAppBarTitleText('Request Verification'),
       ),
-      children: [
-        Center(
-            child: Container(
-          margin: const EdgeInsets.only(top: 50),
-          child: CircleAvatar(
-            backgroundColor: Colors.white,
-            radius: 50,
-            foregroundImage:
-                identity.avatar != null ? identity.avatar!.image : null,
-            child: const SizedBox.expand(
-              child: FittedBox(
-                fit: BoxFit.fill,
-                child: Icon(Icons.person),
-              ),
-            ),
-          ),
-        )),
-        Center(
-            child: Container(
-                margin: const EdgeInsets.only(bottom: 10, top: 10),
-                child: Text(
-                  identity.username,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 32,
-                    color: Colors.white,
+      children: _link == null
+          ? [
+              const SizedBox(height: 100),
+              const Center(child: CircularProgressIndicator()),
+            ]
+          : [
+              Center(
+                  child: Container(
+                margin: const EdgeInsets.only(top: 50),
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 50,
+                  foregroundImage:
+                      identity.avatar != null ? identity.avatar!.image : null,
+                  child: const SizedBox.expand(
+                    child: FittedBox(
+                      fit: BoxFit.fill,
+                      child: Icon(Icons.person),
+                    ),
                   ),
-                ))),
-        const Center(
-            child: Text(
-          "Wants you to verify this claim",
-          style: TextStyle(
-            fontWeight: FontWeight.w300,
-            fontSize: 20,
-            color: Colors.grey,
-          ),
-        )),
-        const SizedBox(height: 10),
-        Center(
-            child: Text(
-          models.ClaimType.claimTypeToString(claim.claim.claimType),
-          style: const TextStyle(
-            fontWeight: FontWeight.w200,
-            fontSize: 24,
-            color: Colors.white,
-          ),
-        )),
-        const SizedBox(height: 10),
-        ...shared_ui.renderClaim(claim),
-        const SizedBox(height: 5),
-        Center(
-          child: QrImage(
-            backgroundColor: Colors.white,
-            data: encodedClaim,
-            version: QrVersions.auto,
-            size: 200.0,
-          ),
-        ),
-        const SizedBox(height: 15),
-        const Center(
-            child: Text(
-          "Scan to Verify",
-          style: TextStyle(
-            fontWeight: FontWeight.w200,
-            fontSize: 20,
-            color: Colors.grey,
-          ),
-        )),
-        shared_ui.StandardButtonGeneric(
-          actionText: 'Copy',
-          actionDescription: 'Share this unique code with others to verify',
-          left: shared_ui.makeSVG('content_copy.svg', 'Copy'),
-          onPressed: () async {
-            await handlePresentClipboard(state, pointer, context);
-          },
-        ),
-        shared_ui.StandardButtonGeneric(
-          actionText: 'Share',
-          actionDescription: 'Share code for verification',
-          left: shared_ui.makeSVG('share.svg', 'Share'),
-          onPressed: () async {
-            await handlePresentShare(state, pointer);
-          },
-        ),
-        const SizedBox(height: 15),
-      ],
+                ),
+              )),
+              Center(
+                  child: Container(
+                      margin: const EdgeInsets.only(bottom: 10, top: 10),
+                      child: Text(
+                        identity.username,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 32,
+                          color: Colors.white,
+                        ),
+                      ))),
+              const Center(
+                  child: Text(
+                "Wants you to verify this claim",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              )),
+              const SizedBox(height: 10),
+              Center(
+                  child: Text(
+                models.ClaimType.claimTypeToString(claim.claim.claimType),
+                style: const TextStyle(
+                  fontWeight: FontWeight.w200,
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              )),
+              const SizedBox(height: 10),
+              ...shared_ui.renderClaim(claim),
+              const SizedBox(height: 5),
+              Center(
+                child: QrImage(
+                  backgroundColor: Colors.white,
+                  data: _link!,
+                  version: QrVersions.auto,
+                  size: 200.0,
+                ),
+              ),
+              const SizedBox(height: 15),
+              const Center(
+                  child: Text(
+                "Scan to Verify",
+                style: TextStyle(
+                  fontWeight: FontWeight.w200,
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              )),
+              shared_ui.StandardButtonGeneric(
+                actionText: 'Copy',
+                actionDescription:
+                    'Share this unique code with others to verify',
+                left: shared_ui.makeSVG('content_copy.svg', 'Copy'),
+                onPressed: () async {
+                  await handlePresentClipboard(state, _link!, context);
+                },
+              ),
+              shared_ui.StandardButtonGeneric(
+                actionText: 'Share',
+                actionDescription: 'Share code for verification',
+                left: shared_ui.makeSVG('share.svg', 'Share'),
+                onPressed: () async {
+                  await handlePresentShare(state, _link!);
+                },
+              ),
+              const SizedBox(height: 15),
+            ],
     );
   }
 }
